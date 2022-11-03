2fbe7f8d9f40f7b4254f3812b2ee1cddcc575452
Leonardo_Lo_medico <leonardo.lomedico@hotmail.it>
2022-11-03 12:19:25 +0100

    ESERCIZIO1_CON_BONUS

--- StringSplitter.js
+++ StringSplitter.js
@@ -20,18 +20,39 @@ let StringSplitter = class {
   getSplits(numberOfLetters) {
     let result = " ";
     for (let i = 0; i <= 2; i++) {
-      result = result + this.toBeSplit.substring(i, 2) + " ,";
+      result = result + this.toBeSplit.substr(i, 2) + " ,";
     }
     return result;
   }
   getAllSplits() {
-    let result = "";
+    let result = [];
     for (let i = 0; i < this.toBeSplit.length; i++) {
       for (let j = i + 1; j <= this.toBeSplit.length; j++) {
-        result = result + this.toBeSplit.substring(i, j) + " ,";
+        // result = result + this.toBeSplit.substring(i, j) + " ,";
+        result.push(this.toBeSplit.substring(i, j));
       }
     }
     return result;
   }
+  compare(string1, string2) {
+    
+    let result = string1.filter(element => string2.includes(element) );
+    
+    /*if (string1.length < string2.length) {
+        dim = string1.length;
+    }else{     
+        dim = string2.length
+    }
+      console.log("dim " + dim);
+      for (let i = 0; i < dim; i++) {
+        for (let j = 0; j < dim; j++)
+          if (string1[i] == string2[j]) {
+            equal.push(string1[i]);
+          }
+      }*/
+    
+    return result;
+  }
+
 };
 module.exports = StringSplitter;
--- mainStringSplitter.js
+++ mainStringSplitter.js
@@ -1,5 +1,14 @@
 const StringSplitter = require("./StringSplitter");
 const stringToSplit= new StringSplitter("ciao");
+const bomba = new StringSplitter("bomba");
+const bombay = new StringSplitter("bombay");
 
 console.log(stringToSplit.getSplits(2));
 console.log(stringToSplit.getAllSplits());
+console.log(bomba.getAllSplits());
+console.log(bombay.getAllSplits());
+let arrayBomba = bomba.getAllSplits();
+let arrayBombay = bombay.getAllSplits();
+
+console.log(bomba.compare(arrayBomba,arrayBombay));
+console.log(bombay.compare(arrayBombay,arrayBomba));
